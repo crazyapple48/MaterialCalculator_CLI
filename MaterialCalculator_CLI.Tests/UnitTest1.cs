70b2da2a3f4c0d892ab212770ff514a9c68534e6
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2026-07-30 13:05:15 -0700

    start writing unit tests

---
+++ MaterialCalculator_CLI.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using MaterialCalculator_CLI.CaseFormulas;
+
+namespace MaterialCalculator_CLI.Tests;
+
+public class UnitTest1
+{
+    private double _l = 8;
+    private double _w = 25;
+    
+    [Fact]
+    public void ReactionAtVmax_ReturnsDouble()
+    {
+        var result = SimpleBeamUniformLoad.ReactionAtVmax(_w, _l);
+
+        Assert.Equal(100, result);
+    }
+}
